408a6a896cc9b14dc10f6d5653b58740b292a799
Gabriel Lebec <glebec@gmail.com>
2016-04-22 15:35:23 -0400

    deleted files

--- server/db/models/index.js
+++
@@ -1,8 +0,0 @@
-// Require our models -- these should register the model into mongoose
-// so the rest of the application can simply call mongoose.model('User')
-// anywhere the User model needs to be used.
-
-require('./user.model.js');
-require('./order.model.js');
-require('./product.model.js');
-require('./reviews.model.js');
